a6da7682ccef170a01747a92b46ca923b7bb2ce0
Yasser Fraji <yasserfraji@outlook.com>
2024-03-21 11:48:57 +0100

    Ajoutez des fichiers projet.

---
+++ GestBanque/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GestBanque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
